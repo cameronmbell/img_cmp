4b6159789aadbc1b0e48c50fc390e79a56552c24
Cameron Bell <thedesignerpenguin@gmail.com>
2019-12-01 14:10:55 +1100

    refactored eval

--- include/test.hpp
+++ include/test.hpp
@@ -1,8 +1,8 @@
 #ifndef TEST_HPP
 #define TEST_HPP
 
-#include <string>
-#include <list>
+#include <iostream>
+#include <sstream>
 
 // -- 
 // for unit testing, like a very simple version of catch2
@@ -17,78 +17,107 @@
 // ie if f(x) = x then EVAL(f(2) == f(1)) --> "1 == 2"
 #define EVAL(...) __test_details::eval( \
     __DETAILS_STRINGIFY(__VA_ARGS__), \
-    (__test_details::TermCapture() << __VA_ARGS__).get_string())
-    // ie TermCapture << lhs [operator] rhs
-    // -> LhsTerm<decltype(lhs)> [operator] rhs
-    // -> UBExpr<decltype(lhs), decltype(rhs)>
+    __test_details::Stringable::output_stringable(__test_details::CaptureExpr() << __VA_ARGS__))
+    // ie CaptureExpr << lhs [operator] rhs
+    // -> UnaryExpr<decltype(lhs)> [operator] rhs
+    // -> BinaryExpr<decltype(lhs), decltype(rhs)>
 
 namespace __test_details {
     struct None { };
 
-    template <typename T> struct TrueType : std::true_type { };
-    template <typename T> struct FalseType : std::false_type { };
-
     struct Stringable { 
-        virtual std::string get_string()=0;
+        virtual void write_string(std::ostream& output)=0;
+
+        static std::string output_stringable(Stringable&& s) {
+            std::ostringstream output_ss;
+
+            output_ss << std::boolalpha;
+
+            s.write_string(output_ss);
+
+            return output_ss.str();
+        }
     };
 
+    template <typename T> struct TrueType : public std::true_type { };
+    template <typename T> struct FalseType : public std::false_type { };
+
     template <typename L, typename R>
-    class UBExpr : Stringable {
+    class BinaryExpr : public Stringable {
         const char* m_op;
         L m_l;
         R m_r;
 
     public:
-        UBExpr(L l, const char* op, R r) 
+        BinaryExpr(L l, const char* op, R r) 
             : m_l(l), m_r(r), m_op(op) { }
 
-        std::string get_string() {
-            return std::to_string(m_l) + m_op + std::to_string(m_r);
+        /* override */ void write_string(std::ostream& output) {
+            output << m_l << ' ' << m_op << ' ' << m_r;
         }
 
+#define __DETAILS_TEST_BINEXPROPERRMSG \
+    "attempt to use relational operator on binary expression to form a trinarary expression" \
+    "failed because only unary and binary expressions are supported by the test system i.e." \
+    "all expressions must take the form (a) [op] (b)"
+
         template <typename T>
-        constexpr void operator==(T const& t) { static_assert(TrueType<T>::value, "bad expr"); }
+        void operator==(T const& t) { static_assert(FalseType<T>::value, __DETAILS_TEST_BINEXPROPERRMSG); }
         template <typename T>
-        constexpr void operator!=(T const& t) { static_assert(TrueType<T>::value, "bad expr"); }
+        void operator!=(T const& t) { static_assert(FalseType<T>::value, __DETAILS_TEST_BINEXPROPERRMSG); }
         template <typename T>
-        constexpr void operator>=(T const& t) { static_assert(TrueType<T>::value, "bad expr"); }
+        void operator>=(T const& t) { static_assert(FalseType<T>::value, __DETAILS_TEST_BINEXPROPERRMSG); }
         template <typename T>
-        constexpr void operator<=(T const& t) { static_assert(TrueType<T>::value, "bad expr"); }
+        void operator<=(T const& t) { static_assert(FalseType<T>::value, __DETAILS_TEST_BINEXPROPERRMSG); }
         template <typename T>
-        constexpr void operator>(T const& t) { static_assert(TrueType<T>::value, "bad expr"); }
+        void operator>(T const& t) { static_assert(FalseType<T>::value, __DETAILS_TEST_BINEXPROPERRMSG); }
         template <typename T>
-        constexpr void operator<(T const& t) { static_assert(TrueType<T>::value, "bad expr"); }
+        void operator<(T const& t) { static_assert(FalseType<T>::value, __DETAILS_TEST_BINEXPROPERRMSG); }
+        template <typename T>
+        void operator&&(T const& t) { static_assert(FalseType<T>::value, __DETAILS_TEST_BINEXPROPERRMSG); }
+        template <typename T>
+        void operator||(T const& t) { static_assert(FalseType<T>::value, __DETAILS_TEST_BINEXPROPERRMSG); }
     };
 
     template <typename L>
-    class LhsTerm : Stringable {
+    class UnaryExpr : public Stringable { 
         L m_l;
 
     public:
-        LhsTerm(L l) : m_l(l) { }
+        UnaryExpr(L l)
+            : m_l(l) { }
 
-        std::string get_string() {
-            return std::to_string(m_l);
+        /* override */ void write_string(std::ostream& output) {
+            output << m_l;
         }
 
+        // when acted upon by a relational operator
+        // a unary expr is converted to a binary expr
         template<typename R>
-        auto operator==(R const& rt) -> UBExpr<L, R> { return UBExpr<L, R>(m_l, "==", rt); }
+        auto operator==(R const& r) -> BinaryExpr<L, R> { return BinaryExpr<L, R>(m_l, "==", r); }
         template<typename R>
-        auto operator!=(R const& rt) -> UBExpr<L, R> { return UBExpr<L, R>(m_l, "!=", rt); }
+        auto operator!=(R const& r) -> BinaryExpr<L, R> { return BinaryExpr<L, R>(m_l, "!=", r); }
         template<typename R>
-        auto operator>=(R const& rt) -> UBExpr<L, R> { return UBExpr<L, R>(m_l, ">=", rt); }
+        auto operator>=(R const& r) -> BinaryExpr<L, R> { return BinaryExpr<L, R>(m_l, ">=", r); }
         template<typename R>
-        auto operator<=(R const& rt) -> UBExpr<L, R> { return UBExpr<L, R>(m_l, "<=", rt); }
+        auto operator<=(R const& r) -> BinaryExpr<L, R> { return BinaryExpr<L, R>(m_l, "<=", r); }
         template<typename R>
-        auto operator>(R const& rt) -> UBExpr<L, R> { return UBExpr<L, R>(m_l, ">", rt); }
+        auto operator>(R const& r) -> BinaryExpr<L, R> { return BinaryExpr<L, R>(m_l, ">", r); }
         template<typename R>
-        auto operator<(R const& rt) -> UBExpr<L, R> { return UBExpr<L, R>(m_l, "<", rt); }
+        auto operator<(R const& r) -> BinaryExpr<L, R> { return BinaryExpr<L, R>(m_l, "<", r); }
+
+        template<typename R>
+        auto operator&&(R const& r) -> BinaryExpr<L, R> { return BinaryExpr<L, R>(m_l, "&&", r); }
+        template<typename R>
+        auto operator||(R const& r) -> BinaryExpr<L, R> { return BinaryExpr<L, R>(m_l, "||", r); }
+
+        // all other operators are implicitly private
     };
 
-    struct TermCapture {
+    struct CaptureExpr {
         template<typename T>
-        auto operator<<(T const& lhs) -> LhsTerm<T> {
-            return LhsTerm<T>(lhs);
+        auto operator<<(T const& lhs) -> UnaryExpr<T> {
+            return UnaryExpr<T>(lhs);
         }
     };
 
